db8e66f6007e4bc1bb96f7ce04dedf76c6e78d8e
Anton Zavalov <fort.malvery@gmail.com>
2020-03-11 00:47:52 +0500

    - implement rules.fieldProperties.*

--- src/main/kotlin/kvalidator/rules/fieldProperties.kt
+++ src/main/kotlin/kvalidator/rules/fieldProperties.kt
@@ -1,49 +1,80 @@
 package kvalidator.rules
 
+import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonLiteral
+import kotlinx.serialization.json.JsonNull
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.boolean
 
 class Required : Rule() {
     override val name: String = "required"
     override fun validate(data: JsonObject?, attribute: String): Boolean {
-        TODO("Not yet implemented")
-        // if (data == null) return false
-        // if (!data.containsKey(attribute)) return false
-        //
-        // return when (val element = data[attribute]) {
-        //     is JsonLiteral -> {
-        //         when {
-        //             element.isString -> element.content.length.toDouble() > 0
-        //             element.doubleOrNull != null -> true
-        //             element.booleanOrNull != null -> true
-        //             else -> false
-        //         }
-        //     }
-        //     else -> false
-        // }
+        if (data == null) return false
+        if (!data.containsKey(attribute)) return false
+
+        return when (val element = data[attribute]) {
+            is JsonNull -> false
+            is JsonLiteral -> {
+                when {
+                    element.isString -> element.content.length.toDouble() > 0
+                    else -> true
+                }
+            }
+            is JsonArray -> {
+                when {
+                    element.isNotEmpty() -> true
+                    else -> false
+                }
+            }
+            is JsonObject -> {
+                when {
+                    element.isNotEmpty() -> true
+                    else -> false
+                }
+            }
+            else -> false
+        }
     }
 }
 
 class Confirmed : Rule() {
     override val name: String = "confirmed"
     override fun validate(data: JsonObject?, attribute: String): Boolean {
-        TODO("Not yet implemented")
+        if (data == null) return false
+        if (!data.containsKey(attribute)) return false
+
+        val confirmAttr = attribute + "_confirmation"
+        if (!data.containsKey(confirmAttr)) return false
+        val elementConfirm = data[confirmAttr]
+
+        return when (val element = data[attribute]) {
+            is JsonLiteral -> {
+                when (element) {
+                    elementConfirm -> true
+                    else -> false
+                }
+            }
+            else -> false
+        }
     }
 }
 
 class Accepted : Rule() {
     override val name: String = "accepted"
     override fun validate(data: JsonObject?, attribute: String): Boolean {
-        return when (val element = data?.get(attribute)) {
-            is JsonPrimitive -> {
-                val c = data as JsonLiteral
+        if (data == null) return false
+        if (!data.containsKey(attribute)) return false
+
+        return when (val element = data[attribute]) {
+            is JsonLiteral -> {
                 when {
-                    c.isString && element.content == "yes" -> true
-                    c.isString && element.content == "no" -> true
-                    c.isString && element.content == "1" -> true
-                    data.boolean -> data.boolean
-                    data.doubleOrNull != null && data.double == 1.00 -> true
+                    element.isString && element.content == "true" -> true
+                    element.isString && element.content == "yes" -> true
+                    element.isString && element.content == "on" -> true
+                    element.isString && element.content == "1" -> true
+                    element.contentOrNull == "true" -> true
+                    element.doubleOrNull != null && element.double == 1.00 -> true
                     else -> false
                 }
             }
