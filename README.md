b937f663bd6aa190b87017d53519ed3bfcef9cd8
dasha <marifeta1@mail.ru>
2020-04-01 21:32:03 +0300

    add info

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# kvalidator
+# **kvalidator**
 
 ## Why use kvalidator?
 
@@ -13,20 +13,29 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.marifeta:kvalidator:0.0.1")
+    implementation("com.github.Marifeta:kvalidator:0.0.1")
 }
 ```
 
 ## Usage
 
+#### Validate
 ```kt
 
+val testJson = JsonObject(mapOf(
+        "name" to JsonPrimitive("John"),
+        "age" to JsonPrimitive(28),
+        "isHuman" to JsonPrimitive(true),
+))
+
 val rules = mapOf<String, List<Rule>>(
-        "one_number" to listOf(Between(5, 30)),
-        "one_string" to listOf(Between(3, 7))
+        "name" to listOf(Alpha(), Required()),
+        "age" to listOf(IsInteger(), Between(25, 48)),
+        "isHuman" to listOf(IsBoolean()),
 )
 
-val validator = Validator(somethingKotlinJsonData, rules)        
+val validator = Validator(testJson, rules)
+validator.validate() // true 
 
 if(!validator.validate()) {
     validator.errors.forEach {(attribute, messages)->
@@ -37,3 +46,55 @@ if(!validator.validate()) {
 }
 
 ```
+#### Errors
+```kt
+
+val firstError = validator.errors.first() // firstError or null
+val validator.errors.all() // mutableListOf(reason) or null
+val errorCount = validator.errors.count // errors.size (int)
+
+```
+## Available Rules
+
+#### Alpha
+ The field under validation must be entirely alphabetic characters.
+####  AlphaNum
+ The field under validation must be entirely alpha-numeric characters.
+#### AlphaDash
+ The field under validation may have alpha-numeric characters, as well as dashes and underscores.
+#### IsInteger
+ The field under validation must have an integer value.
+#### IsString
+ The field under validation must be a string.
+#### IsBoolean
+ The field under validation must be a boolean value of the form true, false (Not String).
+#### IsArray
+ The field under validation must be an array.
+#### IsNumeric
+ Validate that an attribute is numeric. The string representation of a number will pass.
+#### IsDate
+ The field under validation must be a valid date format which is acceptable by Java's LocalDate object in format "yyyy-mm-ddThh:mm:ss".
+#### AfterDate
+ The field under validation must be after the given date.
+#### Required
+ Checks if the length of the String representation of the value is >
+#### Confirmed
+ The field under validation must have a matching field of foo_confirmation. For example, if the field under validation is password, a matching password_confirmation field must be present in the input.
+#### Accepted
+ The field under validation must be yes, on, 1 or true. This is useful for validating "Terms of Service" acceptance.
+#### Between
+ The field under validation must have a size between the given min and max. Strings, numerics, and files are evaluated in the same fashion as the size rule.
+#### Min
+ Validate that an attribute is at least a given size.
+#### Max
+ Validate that an attribute is no greater than a given size
+#### Size
+ The field under validation must have a size matching the given value. For string data, value corresponds to the number of characters. For numeric data, value corresponds to a given integer value.
+#### Url
+ Validate that an attribute has a valid URL format
+#### Email
+ The field under validation must be formatted as an e-mail address.
+
+
+## License
+ MIT
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,24 @@
+
+import java.util.Date
+
+val LIBRARY_VERSION_NAME = "0.0.1"
+val GROUP_ID = "com.github.Marifeta"
+val ARTIFACT_ID = rootProject.name
+val BINTRAY_REPOSITORY = "generic"
+val BINTRAY_ORGINIZATION = "murphy"
+val KOTLINX_SERIALIZATION_RUNTIME = "0.14.0"
+val SHORT_DESC = """
+    Validator for kotlin json serialization.
+""".trimIndent()
+val VCS_URL = "https://github.com/Marifeta/kvalidator.git"
+val WEBSITE_URL = "https://github.com/Marifeta/kvalidator"
+val ISSUE_TRACKER_URL = "https://github.com/Marifeta/kvalidator/issues"
+val CONTACT_EMAIL = "diksynox@gmail.com"
+
 buildscript {
     // ext.kotlin_version = '1.3.60'
     repositories { jcenter() }
-
+    val kotlin_version = "1.3.60"
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.60")
         classpath("org.jetbrains.kotlin:kotlin-serialization:1.3.60")
@@ -16,14 +33,112 @@ buildscript {
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.3.41"
-
-    // Apply the java-library plugin for API and implementation separation.
-    `java-library`
+    id("maven-publish")
+    id("com.jfrog.bintray") version "1.8.4"
 }
 apply(plugin = "kotlinx-serialization")
 
+group = GROUP_ID
+version = LIBRARY_VERSION_NAME
+
+tasks.bintrayUpload {
+    dependsOn("publishToMavenLocal")
+}
+
+tasks.register<Jar>("sourcesAll") {
+    from(sourceSets.main.get().allSource)
+    archiveClassifier.set("sources")
+}
+
+tasks.withType<GenerateMavenPom>().configureEach {
+    val matcher = Regex("""generatePomFileFor(\w+)Publication""").matchEntire(name)
+    val publicationName = matcher?.let { it.groupValues[1] }
+    destination = file("$buildDir/poms/$publicationName-pom.xml")
+}
+
+tasks.register<Jar>("javadocJar") {
+    archiveClassifier.set("javadoc")
+    from("$rootDir/README.md")
+}
+
+fun printResults(desc: TestDescriptor, result: TestResult) {
+    if (desc.parent != null) {
+        val output = result.run {
+            "Results: $resultType (" +
+                    "$testCount tests, " +
+                    "$successfulTestCount successes, " +
+                    "$failedTestCount failures, " +
+                    "$skippedTestCount skipped" +
+                    ")"
+        }
+        val testResultLine = "|  $output  |"
+        val repeatLength = testResultLine.length
+        val seperationLine = "-".repeat(repeatLength)
+        println(seperationLine)
+        println(testResultLine)
+        println(seperationLine)
+    }
+}
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifact(tasks["sourcesAll"])
+            artifact(tasks["javadocJar"])
+            pom {
+                name.set(provider { "$GROUP_ID:$ARTIFACT_ID" })
+                description.set(provider { project.description ?: SHORT_DESC })
+                url.set(VCS_URL)
+                developers {
+                    developer {
+                        name.set(BINTRAY_ORGINIZATION)
+                        email.set(CONTACT_EMAIL)
+                    }
+                }
+                scm {
+                    connection.set(VCS_URL)
+                    developerConnection.set(VCS_URL)
+                    url.set(ISSUE_TRACKER_URL)
+                }
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("http://www.opensource.org/licenses/mit-license.php")
+                    }
+                }
+            }
+        }
+    }
+}
+
+bintray {
+    user = if(project.hasProperty("user_name")) project.property("user_name").toString() else ""
+    key = if(project.hasProperty("apikey")) project.property("apikey").toString() else ""
+    publish = true
+    override = false
+    setPublications("mavenJava")
+    pkg.apply {
+        repo = BINTRAY_REPOSITORY
+        name = ARTIFACT_ID
+        userOrg = BINTRAY_ORGINIZATION
+        desc = SHORT_DESC
+        setLicenses("MIT")
+        version.apply {
+            name = LIBRARY_VERSION_NAME
+            vcsTag = LIBRARY_VERSION_NAME
+            released = Date().toString()
+        }
+
+        vcsUrl = VCS_URL
+        websiteUrl = WEBSITE_URL
+        issueTrackerUrl = ISSUE_TRACKER_URL
+
+        setLabels("kotlin", "kvalidator", "serialization", "jvm", "validator")
+    }
+}
+
 repositories {
-    // Use jcenter for resolving dependencies.
+    // Use jcenter for resolving your dependencies.
     // You can declare any Maven/Ivy/file repository here.
     jcenter()
 }
@@ -40,6 +155,6 @@ dependencies {
 
     // Use the Kotlin JUnit integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.14.0") // JVM dependency
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:$KOTLINX_SERIALIZATION_RUNTIME")
 
 }
